49bcd2228d9fa3cefcc3cc4be60bb0b2b69a9975
DiegoDiazR <diegonicolas2640@gmail.com>
2026-03-12 16:11:02 -0500

    Se aplican reglas de seguridad para no incluir archivos importantes al repositorio

--- backend_backup/app/models/__init__.py
+++
@@ -1,7 +0,0 @@
-from .asset import Asset
-from app.models.asset import Asset  # si existe
-# from app.models.rbi_component import RBIComponent
-# from app.models.rbi_581_analysis import RBI581Analysis
-# from app.models.dm_thinning import DMThinning
-
-from app.models.rbi581_thinning_result import RBI581ThinningResult
